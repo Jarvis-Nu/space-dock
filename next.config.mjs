830fe37fd302224a3514d84fea80873e5360b340
okhaimie-dev <oktonystark8@gmail.com>
2023-08-05 09:56:45 -0500

    fixing pipeline

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,8 @@ const nextConfig = {
   reactStrictMode: true,
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
   },
 }
 
-export default nextConfig
+export default nextConfig;
