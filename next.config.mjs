3b36ed94a51d3b0ad125f348ac31aeca43d31c7b
okhaimie-dev <oktonystark8@gmail.com>
2023-08-05 00:16:34 -0500

    action retry

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import { config } from 'process'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
@@ -8,9 +6,6 @@ const nextConfig = {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
-  },
-  experimental: {
-    appDir: true,
   },
 }
 
