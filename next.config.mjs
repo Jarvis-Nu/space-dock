3d7293f9f3fe52365376843e1ad57ded8ffc6db2
okhaimie-dev <oktonystark8@gmail.com>
2023-08-05 10:00:52 -0500

    prep progress to show to mark

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,9 @@ const nextConfig = {
   reactStrictMode: true,
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
   },
 }
 
-export default nextConfig;
+export default nextConfig
