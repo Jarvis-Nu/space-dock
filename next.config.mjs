46e7113a88e399a668b63de0354331af2e819463
okhaimie-dev <oktonystark8@gmail.com>
2023-08-04 23:59:24 -0500

    Git actions: replaced package manager

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import { config } from 'process'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
