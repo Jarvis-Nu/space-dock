28acb190ead7a5ed1a7256de9f9699dfe8c34a28
supreme2580 <victoromorogbe69@gmail.com>
2023-08-06 17:36:03 +0100

    suprememods

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+import Image from "next/image"
+import Link from "next/link"
+import { ConnectKitButton } from "connectkit"
 
 // Refactor to allow for dynamic metadata @okhaimie-dev: Currently clashes with server component.
 // export const metadata: Metadata = {
@@ -43,7 +46,31 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <Providers>
               <div className="relative flex flex-col min-h-screen">
-                <div className="flex-1">{children}</div>
+                <div className="w-full h-full">
+                  <div className="w-full h-screen bg-[url('/gradient.png')]">
+                    <div className="w-full h-full bg-[url('/circles.png')] bg-no-repeat bg-center object-cover bg-cover">
+                      <div className="flex justify-center w-full">
+                        <nav className="flex items-center justify-between w-full max-w-6xl pt-5">
+                          <div className="w-[225px] h-[40px] relative">
+                            <Image src={"/space-dock.png"} layout="fill" alt="Space-Dock logo" />
+                          </div>
+                          <div className="flex items-center space-x-10">
+                            <div className="text-xl font-medium text-dark">
+                              <Link href={"/"}>Mission</Link>
+                            </div>
+                            <div className="text-xl font-medium text-dark">
+                              <Link href={"/"}>Community</Link>
+                            </div>
+                            <div>
+                              <ConnectKitButton />
+                            </div>
+                          </div>
+                        </nav>
+                      </div>
+                      <div className="flex justify-center w-full h-full">{children}</div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </Providers>
             <TailwindIndicator />
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,7 @@
-"use client"
-
-import { ConnectKitButton } from "connectkit";
-import Image from "next/image";
-import Link from "next/link";
-
 export default function IndexPage() {
   return (
-    <div className="w-full h-full">
-      <div className="w-full h-screen bg-[url('/gradient.png')]">
-        <div className="w-full h-full bg-[url('/circles.png')] bg-no-repeat bg-center object-cover bg-cover">
-          <div className="flex justify-center w-full">
-            <nav className="flex items-center justify-between w-full max-w-6xl pt-5">
-              <div className="w-[225px] h-[40px] relative">
-                <Image src={"/space-dock.png"} layout="fill" alt="Space-Dock logo" />
-              </div>
-              <div className="flex space-x-10">
-                <div className="text-xl font-medium text-dark">
-                  <Link href={"/"}>Mission</Link>
-                </div>
-                <div className="text-xl font-medium text-dark">
-                  <Link href={"/"}>Community</Link>
-                </div>
-                <div>
-                  <ConnectKitButton />
-                </div>
-              </div>
-            </nav>
-          </div>
-        </div>
-      </div>
+    <div className="w-full max-w-6xl">
+      Hi
     </div>
   )
 }
